3c04f986475db9f0223adb86caf422ecd74bcabc
wangweijun <wangweijun@qmtv.com>
2018-03-12 14:28:13 +0800

    update code

--- example.js
+++
@@ -1,3 +0,0 @@
-var dropdown = new pureCssDropdown("dropdown");
-var dropdown1 = new pureCssDropdown("dropdown1");
-var dropdown2 = new pureCssDropdown("dropdown2");
---
+++ js/main.js
@@ -0,0 +1,26 @@
+function getJson(element,jsonUrl){
+	this.element = element;
+	this.jsonUrl = jsonUrl;
+	var select_content = element.children(".select_content");
+	var select_list = element.children(".select_content").children(".select_list");
+	var loading_box = element.children(".select_content").children(".loading_box");
+	element.click(function(){
+		select_list.html("");
+		select_content.toggle();
+		loading_box.toggle();
+		$.ajax({
+			url: jsonUrl,
+			success: function(data){
+				loading_box.hide();
+				for(var key in data){
+					select_list.append("<li value=" + data[key] + " onclick=select(this.value,this,this.innerHTML)>" + key + "</li>")
+				}
+			}
+		})
+	})
+};
+function select(e,c,d){
+	var content = $(c).parent().parent().prev();
+	content[0].innerHTML = d;
+	content[0].value = e
+}
